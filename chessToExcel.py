bc1cd1e3f3b76ee7ebf754cf938e39d564f63beb
masonadams25 <adams.mason25@gmail.com>
2023-07-30 01:47:11 -0500

    Added excel printing function

--- chessToExcel.py
+++ chessToExcel.py
@@ -1,4 +1,5 @@
 from chessdotcom import get_player_games_by_month
+import openpyxl
 from time import strftime, localtime
 
 username = "Masonrules9"
@@ -9,7 +10,7 @@ games = response.json['games']
 
 dailyGames = {}
 def addDailyGames(dict, result, month, day, rating, accuracy):
-    dict['date'] = str(day) + '-' + str(month)
+    dict['date'] = str(month) + '-' + str(day)
     dict['totGames'] = int(dict['totGames']) + 1
     dict['avgRating'] = (int(dict['avgRating']) * (int(dict['totGames'])-1) + int(rating)) / int(dict['totGames'])
     if result == 'win':
@@ -19,6 +20,43 @@ def addDailyGames(dict, result, month, day, rating, accuracy):
     dict['winPercent'] =  (float(dict['wins']) / float(dict['totGames']))*100
     if accuracy != 'N/A':
         dict['avgAccuracy'] = (int(dict['avgAccuracy']) * (int(dict['totGames'])-1) + int(accuracy)) / int(dict['totGames'])
+
+def writeToExcel(dict):
+    path = "chessToExcel.xlsx"
+    try:
+        wb = openpyxl.load_workbook(path)
+        print("Excel file loaded")
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
+        print("Excel file created")
+        print("Path: " + path)
+    sheet = wb.active
+
+    sheet.cell(row = 1, column = 1).value = "Date"
+    sheet.cell(row = 2, column = 1).value = "Avg Rating"
+    sheet.cell(row = 3, column = 1).value = "Games"
+    sheet.cell(row = 4, column = 1).value = "Wins"
+    sheet.cell(row = 5, column = 1).value = "Losses"
+    sheet.cell(row = 6, column = 1).value = "Win %"
+    sheet.cell(row = 7, column = 1).value = "Avg acc"
+
+    print("Excel file saved")
+
+    count = 2
+    for day in dict:
+        sheet.cell(row = 1, column = count).value = str(dict[day]['date'])
+        sheet.cell(row = 2, column = count).value = str(round(float(dailyGames[day]['avgRating']),2))
+        sheet.cell(row = 3, column = count).value = str(dailyGames[day]['totGames'])
+        sheet.cell(row = 4, column = count).value = str(dailyGames[day]['wins'])
+        sheet.cell(row = 5, column = count).value = str(dailyGames[day]['losses'])
+        sheet.cell(row = 6, column = count).value = str(round(float(dailyGames[day]['winPercent']),2))
+        sheet.cell(row = 7, column = count).value = str(round(float(dailyGames[day]['avgAccuracy']),2))
+        
+        count = count + 1
+
+    wb.save(path)
+        
+
     
 for game in games:
     if game['white']['username'] == username:
@@ -53,6 +91,7 @@ for game in games:
 
     #print(str(result).ljust(10) + " " + str(accuracy).ljust(6) + " " + str(rating).ljust(4) + " " + str(date).ljust(10))
 
+writeToExcel(dailyGames)
 for day in dailyGames:
     print(str(dailyGames[day]['date']).ljust(6) + 
           "- Avg rating: " + str(round(float(dailyGames[day]['avgRating']),2)).ljust(7) +
